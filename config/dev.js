2f5ff037ee053757345222fbed9c9d8a1e3793f6
Kushagra Sharma <kushagra8888@gmail.com>
2019-01-20 21:09:18 +0530

    trais config

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://admin:admin123@ds135974.mlab.com:35974/blog_dev',
+  cookieKey: '123123123',
+  redisURL: 'reids://127.0.0.1:6379'
 };
